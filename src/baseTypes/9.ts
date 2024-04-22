d21b484a91b0a88f9a0b5a43b1e7952271bbeeb4
FourSage747 <129331961+FourSage747@users.noreply.github.com>
2024-04-22 15:06:32 +0200

    дз2

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,7 +2,11 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
+type Object = {
+  [key: string]: string | number | string[] | { createAt: Date; updateAt: Date };
+}
+
+const page1: Object = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -13,7 +17,7 @@ const page1 = {
   }
 }
 
-const page2 = {
+const page2: Object = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
--- src/generic/8.ts
+++ src/generic/8.ts
@@ -19,6 +19,14 @@ type Form = {
 };
 
 // Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
-type Params = Form;
+type Params = Omit<Form, 'errors'>;
+
+const params: Params = {
+  email: 'example@example.com',
+  firstName: 'John',
+  lastName: 'Doe',
+  phone: '1234567890',
+  //errors: {}, // Поле errors більше не може бути використане
+};
 
 export {};
